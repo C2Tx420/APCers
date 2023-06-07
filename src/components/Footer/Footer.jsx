438619b80bb0fddea4b944ea56b3b62b5260c1f3
Chung Tu <tuthanhtrung2000@gmail.com>
2023-06-07 21:37:22 +0700

    fix footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,35 +3,35 @@ import './Footer.scss'
 
 export default function Footer() {
     return (
-        <footer className='Footer-big'>
-            <div className='Footer-con'>
-                <div>
-                    <h1 className='APCERS'>APCERS</h1>
-                    <p className='twf'>The world's first and largest digital marketplace for crypto collectibles and non-fungible tokens (NFTs). Buy, sell, and discover exclusive digital items. </p>
+        <footer className='footer container'>
+            <div className="footer__top">
+                <div className='footer__top-item'>
+                    <p className='brand'>APCERS</p>
+                    <p className='desc'>The world's first and largest digital marketplace for crypto collectibles and non-fungible tokens (NFTs). Buy, sell, and discover exclusive digital items. </p>
                 </div>
-                <div>
-                    <h1 className='MP'>Market Place</h1>
-                    <div className='ALL'>
-                        <p>All NFTs</p>
-                        <p>News</p>
-                        <p>Art</p>
-                        <p>Sports</p>
-                        <p>Utility</p>
-                        <p>Music</p>
-                        <p>Domain Name</p>
-                    </div>
+                <div className='footer__top-item'>
+                    <p className='title'>Market Place</p>
+                    <ul className='list'>
+                        <li><a>All NFTs</a></li>
+                        <li><a>News</a></li>
+                        <li><a>Art</a></li>
+                        <li><a>Sports</a></li>
+                        <li><a>Utility</a></li>
+                        <li><a>Music</a></li>
+                        <li><a>Domain Name</a></li>
+                    </ul>
                 </div>
-                <div>
+                <div className='footer__top-item'>
                     <h1 className='MA'>My Account</h1>
-                    <div className='PF'>
-                        <p>Profile</p>
-                        <p>Favorite</p>
-                        <p>My Collections</p>
-                        <p>Settings</p>
-                    </div>
+                    <ul className='PF'>
+                        <li><a>Profile</a></li>
+                        <li><a>Favorite</a></li>
+                        <li><a>My Collections</a></li>
+                        <li><a>Settings</a></li>
+                    </ul>
                 </div>
-                <div>
-                    <h1 className='SITL'>Stay In The Loop</h1>
+                <div className='footer__top-item'>
+                    <p className='SITL'>Stay In The Loop</p>
                     <p className='JOIN'>Join our mailing list to stay in the loop with our newest feature releases, NFT drops, and tips and tricks for navigating NFTs.</p>
                     <div className='REC'><input className='ENTER' /><p> Enter your email address</p></div>
                 </div>
