0ef8d98dd736c08926117227ba3bf022ae9fc49e
Chung Tu <tuthanhtrung2000@gmail.com>
2023-06-08 17:18:20 +0700

    fix signup nft

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './Home.scss'
 import AmazeNFT from '../../components/AmazeNFT/AmazeNFT'
+import SignUpNFT from '../../components/SignUpNFT/SignUpNFT'
 
 export default function Home() {
   return (
     <main className="home">
       <section className="amaze-section container-fluid">
-        <AmazeNFT/>
+        <AmazeNFT />
+      </section>
+      <section className="signup-section container">
+        <SignUpNFT />
       </section>
     </main>
   )
