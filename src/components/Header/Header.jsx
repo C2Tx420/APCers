863a29848086d9d4328fcd7d11fbe31173c42bc0
Chung Tu <tuthanhtrung2000@gmail.com>
2023-06-10 12:15:34 +0700

    upload balance

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,23 +2,54 @@ import { useEffect, useState } from 'react'
 import './Header.scss'
 import Button from '../Button/Button'
 import { WalletService } from '../../services/wallet.service';
+import { Link } from 'react-router-dom';
 export default function Header() {
-    const [walletAddress, setwalletAddress] = useState('');
+    const [walletAddress, setwalletAddress] = useState(()=>{
+        const walletAddress = localStorage.getItem('walletAddress');
+        if(walletAddress) {
+            return walletAddress;
+        } else{
+            return '';
+        }
+    });
+    const [walletBalance,setWalletBalance] = useState('');
     const [loadingConnect, setLoadingConnect] = useState(false);
+
+    useEffect(()=>{
+        (async ()=>{
+            if(walletAddress) {
+                // const balance = await WalletService.getBalance(walletAddress);
+                // console.log(balance.result[0])
+                setWalletBalance({
+                    "address": "5Ab6Q8TL2qgzEQNnnbYYk4SLeKTiV5qRYyoaJ7bCc2v6",
+                    "balance": 100,
+                    "associated_account": "9ZeBHgQoVemfJC2jEt8yRmXetWYhaE1U9ktifVGitUho",
+                    "info": {
+                        "name": "Mungbean",
+                        "symbol": "MBE",
+                        "image": "https://nftstorage.link/ipfs/bafkreifcspzy7hibi7dzo5rxjjfy7nkbfgj2njs2oky5skt4rn7gq2lgoa",
+                        "decimals": 9
+                    }
+                });
+            }
+        })();
+
+    },[])
 
     const handleConnect = async () => {
         setLoadingConnect(true);
-        const walletId = await WalletService.solanaConnect();
-        if(walletId) {
-            setwalletAddress(walletId);
+        const walletAddress = await WalletService.solanaConnect();
+        if (walletAddress) {
+            setwalletAddress(walletAddress);
         }
         setLoadingConnect(false);
-
     }
 
     return (
         <header className="header container">
-            <h1 className="header__brand">APCERS</h1>
+            <Link to={''}>
+                <h1 className="header__brand">APCERS</h1>
+            </Link>
             <ul className="header__menu">
                 <li className="header__menu-item">
                     <a href="#">Marketplace</a>
@@ -32,13 +63,17 @@ export default function Header() {
             </ul>
             <div className="header__btn-group">
                 <Button>Upload</Button>
-                {!walletAddress ?
-                <Button color='bordered' onClick={handleConnect} disable={loadingConnect}>Connect Wallet</Button>
-                 :
-                 <div className="profile">
-                     <p className='profile__walletid'>{walletAddress}</p>
-                 </div>
-                 }
+                {!walletAddress && !walletBalance ?
+                    <Button color='bordered' onClick={handleConnect} disable={loadingConnect}>Connect Wallet</Button>
+                    :
+                    <div className="profile">
+                        <div className="profile__info">
+                            <p>{walletBalance.balance}</p>
+                            <img src={walletBalance.info?.image} alt="profile info image" />
+                        </div>
+                        <p className='profile__walletid'>{walletAddress}</p>
+                    </div>
+                }
             </div>
         </header>
     )
--- src/services/wallet.service.js
+++ src/services/wallet.service.js
@@ -1,32 +1,44 @@
 import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
+import { ApiService } from "./api.service";
+
+const network = import.meta.env.VITE_CURRENT_NETWORK;
+
 
 export const WalletService = {
-    solanaConnect: async () => {
-        const { solana } = window;
-        if (!solana) {
-          alert("Please Install Solana");
-        }
-    
-        try {
-          const network = "devnet";
-          const phantom = new PhantomWalletAdapter();
-          await phantom.connect();
-          const rpcUrl = clusterApiUrl(network);
-          const connection = new Connection(rpcUrl, "confirmed");
-          const wallet = {
-            address: phantom.publicKey.toString(),
-          };
-    
-          if (wallet.address) {
-            // const accountInfo = await connection.getAccountInfo(new PublicKey(wallet.address), "confirmed");
-            // console.log(accountInfo);
-            return wallet.address
-          }
-        }
-        catch (err) {
-          console.log(err);
-        }
-    
+  solanaConnect: async () => {
+    const { solana } = window;
+    if (!solana) {
+      alert("Please Install Solana");
+    }
+
+    try {
+      const network = "devnet";
+      const phantom = new PhantomWalletAdapter();
+      await phantom.connect();
+      const rpcUrl = clusterApiUrl(network);
+      const connection = new Connection(rpcUrl, "confirmed");
+      const wallet = {
+        address: phantom.publicKey.toString(),
+      };
+
+      if (wallet.address) {
+        // const accountInfo = await connection.getAccountInfo(new PublicKey(wallet.address), "confirmed");
+        // console.log(accountInfo);
+        localStorage.setItem("walletAddress", wallet.address);
+        return wallet.address;
       }
-}
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  getBalance: async (walletAddress) => {
+    const getBalanceURL = `https://api.shyft.to/sol/v1/wallet/all_tokens?network=${network}&wallet=${walletAddress}`;
+
+    try {
+        return ApiService.get(getBalanceURL);
+    } catch(e) {
+        throw new Error(e.message);
+    }
+  }
+};
