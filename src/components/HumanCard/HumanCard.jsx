3de1ab93279d7d7da14820e6ce1f26706827b383
Chung Tu <tuthanhtrung2000@gmail.com>
2023-06-12 21:20:18 +0700

    update team list

--- src/components/HumanCard/HumanCard.jsx
+++ src/components/HumanCard/HumanCard.jsx
@@ -1,100 +1,18 @@
 import "./HumanCard.scss";
 import img1 from "../../assets/img/ava1.jpeg";
 
-export default function HumanCard() {
+export default function HumanCard({ img, name, role, desc }) {
   return (
-    <div>
- 
-      <div>
-        <hr className="HumanCard-line1" />
-      </div>
-
-     
-      <div className="HumanCard-about">
-        {" "}
-        <p>ABOUT US</p>
-      </div>
-      <div className="HumanCard-para1">
-        {" "}
-        Welcome our website, this is my team members from APCers{" "}
-      </div>
-    
-     <div className='HumanCard-container'>
-     <div>
-                <div className='HumanCard-box1'>
-                  <div className='Humancard-topbox1'>
-                    <img src={img1} alt="" />
-                    </div>
-                   <div className='Humancard-botbox1'>
-                <p className="Humancard-namebox1">Name:Trần Nhật Mỷ </p>
-                <p>ID: 52210522 </p>
-                <p>Position: TeamLead</p>
-                <p>Contact:</p>
-                 <br />  
-                </div>
-                </div>
-             </div>
-             <div>
-                <div className='HumanCard-box2'>
-                  <div className='Humancard-topbox2'>
-                    <img src={img1} alt="" />
-                    </div>
-                   <div className='Humancard-botbox2'>
-                <p className='Humancard-namebox2'>Name:Trần Nhật Mỷ </p>
-                <p>ID: 52210522 </p>
-                <p>Position: TeamLead</p>
-                <p>Contact:</p>
-                 <br />  
-                </div>
-                </div>
-             </div>
-            {/* member3 */}
-            <div>
-                <div className='HumanCard-box3'>
-                  <div className='Humancard-topbox3'>
-                    <img src={img1} alt="" />
-                    </div>
-                   <div className='Humancard-botbox3'>
-                <p className='Humancard-namebox3'>Name:Trần Nhật Mỷ </p>
-                <p>ID: 52210522 </p>
-                <p>Position: TeamLead</p>
-                <p>Contact:</p>
-                 <br />  
-                </div>
-                </div>
-             </div>
-            {/* member 4 */}
-             <div>
-                <div className='HumanCard-box4'>
-                  <div className='Humancard-topbox4'>
-                    <img src={img1} alt="" />
-                    </div>
-                   <div className='Humancard-botbox4'>
-                <p className='Humancard-namebox4'>Name:Trần Nhật Mỷ </p>
-                <p>ID: 52210522 </p>
-                <p>Position: TeamLead</p>
-                <p>Contact:</p>
-                 <br />  
-                </div>
-                </div>
-             </div>
-            {/* member 5 */}
-            <div>
-                <div className='HumanCard-box5'>
-                  <div className='Humancard-topbox5'>
-                    <img src={img1} alt="" />
-                    </div>
-                   <div className='Humancard-botbox5'>
-                <p className='Humancard-namebox5'>Name:Trần Nhật Mỷ </p>
-                <p>ID: 52210522 </p>
-                <p>Position: TeamLead</p>
-                <p>Contact:</p>
-                 <br />  
-                </div>
-                </div>
-             </div>
-
-
+    <div className="humancard">
+      <img src={`https://drive.google.com/uc?export=view&id=${img}`} alt="" />
+      <div className="humancard__bottom">
+        <p className="name">{name}</p>
+        <div className="role">
+          <p className="role__name">{role}</p>
+        </div>
+        <p className="desc">
+          {desc || 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'}
+        </p>
       </div>
     </div>
   );
