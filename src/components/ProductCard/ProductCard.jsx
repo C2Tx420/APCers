93b9cc422cdb277cc9f4bb80724b9fd3fe4505ba
Chung Tu <tuthanhtrung2000@gmail.com>
2023-06-14 21:06:20 +0700

    update go to detail

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import './ProductCard.scss'
+import Button from '../Button/Button';
+import { useNavigate } from 'react-router-dom';
 
 export default function ProductCard({data}) {
     const nftData = data.nft;
+    const navigate = useNavigate();
+
+    const handleGoTo = (e) => {
+      e.stopPropagation();
+      navigate(`/product/${data.nft_address}`)
+
+    }
   return (
-    <div className="product-card">
+    <div onClick={(e)=>handleGoTo(e)} className="product-card">
         <img src={nftData.image_uri} alt="" />
       <div className="product-card__bottom">
         <p className="name">{nftData.name}</p>
-        <p className="desc">
-          {nftData.description || 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'}
-        </p>
         <p className="price">{data.price}<span>{data.currency_symbol}</span></p>
+        <div className="btn-wrapper">
+          <Button onClick={(e)=>handleGoTo(e)} size='s'>Details</Button>
+        </div>
       </div>
     </div>
   )
