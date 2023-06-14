df0cfe935795cc3f17be7a861921cc4e641f090e
Do Quang Vinh <vinhozgamer12@gmail.com>
2023-06-14 13:14:39 +0700

    NameTag

---
+++ src/components/NameTag/NameTag.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./NameTag.scss"
+import img1 from "../../assets/img/NFT.png"
+export default function () {
+  return (
+    <div className='NameTag-container'>
+        {/* NameTag image */}
+        <div className='NameTag-img'>
+        <img src={img1} />
+        </div>
+        {/* NameTag details */}
+        <div className='NameTag-info'>
+            <p className='NameTag-title'>Ice Cube</p>
+            <p className='NameTag-price'>Price:</p>
+            <p className='NameTag-status'>Available</p>
+        </div>
+        {/* NameTag Sale */}
+        <div className='NameTag-saleoff'>
+          <p className='NameYag-saleoff-percent'>sale 15%</p>
+        
+        </div>
+    </div>
+  )
+}
